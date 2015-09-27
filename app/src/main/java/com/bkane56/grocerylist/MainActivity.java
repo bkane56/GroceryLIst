31a8f7f2f664d2968cb5555405d0692d365568a5
Brian Kane <bkane56@gmail.com>
2015-09-27 16:40:42 -0500

    initial commit

---
+++ app/src/main/java/com/bkane56/grocerylist/MainActivity.java
@@ -0,0 +1,120 @@
+package com.bkane56.grocerylist;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+        implements FragmentDrawer.FragmentDrawerListener, View.OnClickListener {
+
+    private Toolbar mToolbar;
+    private FragmentDrawer drawerFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.raise).setOnClickListener(this);
+        findViewById(R.id.lower).setOnClickListener(this);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        drawerFragment = (FragmentDrawer)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp(R.id.fragment_navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
+        drawerFragment.setDrawerListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDrawerItemSelected(View view, int position) {
+        displayView(position);
+    }
+
+    private void displayView(int position) {
+        Fragment fragment = null;
+        String title = getString(R.string.app_name);
+        switch (position) {
+            case 0:
+                fragment = new ShowListFragment();
+                title = getString(R.string.title_show_list);
+                break;
+            case 1:
+                fragment = new ScanNewFragment();
+                title = getString(R.string.title_scan_new);
+                break;
+            case 2:
+                fragment = new AddNewItemFragment();
+                title = getString(R.string.title_add_new);
+                break;
+            default:
+                break;
+        }
+
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_body, fragment);
+            fragmentTransaction.commit();
+
+            // set the toolbar title
+            getSupportActionBar().setTitle(title);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        View image = findViewById(R.id.img);
+        int elevation = Integer.parseInt(((TextView) findViewById(R.id.label)).getText().toString());
+        if (v.getId() == R.id.raise) {
+            elevation += 5;
+        } else {
+            elevation -= 5;
+        }
+        if (elevation<0) {
+            elevation = 0;
+        } else if (elevation>100) {
+            elevation=100;
+        }
+        image.setElevation(elevation);
+
+        ((TextView) findViewById(R.id.label)).setText(Integer.toString(elevation));
+    }
+}
